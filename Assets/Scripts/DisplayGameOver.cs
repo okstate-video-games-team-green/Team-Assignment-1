e2a12f5a28521b4b30a8898832a4e34b576578ca
maxshort <maxshort@users.noreply.github.com>
2016-02-25 21:02:51 -0600

    Made simple start scene

---
+++ Assets/Scripts/DisplayGameOver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//TODO: The actual gameover stuff
+public class DisplayGameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GameOver() {
+		print ("Game over");
+	}
+
+}
