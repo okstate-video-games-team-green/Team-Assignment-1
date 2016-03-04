e12ff20afce56fd365eb3f842ce9b9e56050e96f
maxshort <maxshort@users.noreply.github.com>
2016-03-03 20:29:39 -0600

    Added skeleton of detecting shot and tracking score

---
+++ Assets/Scripts/DetectShot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectShot: MonoBehaviour {
+
+	public TrackScore scoreTracker;
+	public int scoreIncrAmt;
+
+
+	//TODO: this might need to be OnTrigger
+	public void OnCollision(Collision col){
+		scoreTracker.IncrementScore (scoreIncrAmt);
+	}
+}
---
+++ Assets/Scripts/TrackScore.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TrackScore : MonoBehaviour {
+
+	public Text scoreText; //TODO:drag this into unity editor
+	private int score = 0;
+
+	public void Start(){
+		scoreText.text = score.ToString ();
+	}
+
+	public void IncrementScore (int amount) {
+		//TODO: update score variable & text
+		throw new UnityException("Aaron - TODO");
+	}
+
+	//for end of game situation...
+	public int GetScore(){
+		return score;
+	}
+
+
+}
