526e76dafab91f5079cabbfc2f382f695e8f8365
maxshort <maxshort@users.noreply.github.com>
2016-03-06 18:59:36 -0600

    Added high score ui/logic. Moved score ui slightly so it was visible.

--- Assets/Scripts/DisplayGameOver.cs
+++ Assets/Scripts/DisplayGameOver.cs
@@ -1,16 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 //TODO: The actual gameover stuff
 public class DisplayGameOver : MonoBehaviour {
 
 	public GameObject gameOverPanel;
+	public TrackScore trackScore;
+	public Text highScoreText;
+	private int highScoreValue = 0;
 
 	// Update is called once per frame
 
 	public void GameOver() {
-        print ("Game over");
+		int lastScore = trackScore.GetScore ();
+		if (highScoreValue < lastScore) {
+			highScoreValue = lastScore;
+			highScoreText.text = "You set the high score of " + highScoreValue.ToString ();
+		} else {
+			highScoreText.text = "Your score: " + lastScore.ToString()+ "\nHigh Score:" + highScoreValue.ToString ();
+		}
 		gameOverPanel.SetActive(true);
+		
     }
+
+	void Awake(){
+		DontDestroyOnLoad (this);//for keeping high score in memory
+	}
 
 }
