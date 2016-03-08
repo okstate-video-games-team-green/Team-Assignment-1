a2abb81602dbbc4abfa711508550fe52d854d142
maxshort <maxshort@users.noreply.github.com>
2016-03-07 21:24:02 -0600

    Gun firing sort of

--- Assets/Scripts/FireGun.cs
+++ Assets/Scripts/FireGun.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class FireGun : MonoBehaviour {
 	public GameObject gunBullet;
+	public GameObject gun;
 
 	// Use this for initialization
 	void Start () {
@@ -14,10 +15,15 @@ public class FireGun : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			// Create a bullet object.
 			GameObject bullet = (GameObject)Instantiate (gunBullet);
-			// Set the gun barrel as the parent of the bullet.
-			bullet.transform.parent = transform;  // Set parent.
-			// Set the transform values for the bullet.
-			bullet.transform.localPosition = new Vector3 (0f, 0f, 5.0f);
+			//set bullet to position of gun
+			bullet.transform.position = gun.transform.position + new Vector3(0,1,10);
+			//bullet.AddComponent<Rigidbody> ();
+			float vertForce = gun.transform.parent.rotation.eulerAngles.x*50;
+			float horizForce = gun.transform.parent.rotation.eulerAngles.y*2;
+			bullet.GetComponent<Rigidbody> ().AddForce (new Vector3(horizForce, vertForce, 10000));
+			
+			//bullet.GetComponent<Rigidbody>().AddForce(gun.transform.parent.forward*10000 + gun.transform.up*100000);
+			//bullet.AddComponent<SphereCollider> ();
 		}
 	}
 }
