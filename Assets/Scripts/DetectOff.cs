85015daa1e0caa9558824db756a3d21b6e6a780c
maxshort <maxshort@users.noreply.github.com>
2016-03-10 13:23:38 -0600

    Off detect logic

---
+++ Assets/Scripts/DetectOff.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectOff : MonoBehaviour {
+
+	public int yLevel;
+	private bool notifiedOff = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!notifiedOff && transform.position.y < yLevel) {
+			GetComponentInParent<TrackOffMessages> ().offOccurred ();
+		}
+	}
+		
+}
---
+++ Assets/Scripts/TrackOffMessages.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackOffMessages : MonoBehaviour {
+
+	public TrackScore scoreTracker;
+	public int numberOfOffs; //number of items knocked off pyramid to have score count
+	private int offsLeft;
+
+
+	// Use this for initialization
+	void Start () {
+		offsLeft = numberOfOffs;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void offOccurred() {
+		offsLeft--;
+		if (offsLeft== 0) {
+			scoreTracker.IncrementScore ();
+		}
+	}
+}
